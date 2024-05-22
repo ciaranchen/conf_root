043fb5903496f8cb573a8bd6ae1609023c5382ea
ciaranchen <ciaranchen@qq.com>
2024-05-22 16:54:12 +0800

    前进版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name="conf_root",  # 包名
-    version="0.3.1",  # 版本号
+    version="0.3.2",  # 版本号
     install_requires=[
         "ruamel.yaml"
     ],
