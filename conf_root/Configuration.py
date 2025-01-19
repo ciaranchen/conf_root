e3bf092505be637be8904c64a61a4a39b8a5cc64
ciaranchen <ciaranchen@qq.com>
2025-01-19 18:18:00 +0800

    移除Fields相关代码和测试。

--- conf_root/Configuration.py
+++
@@ -1,7 +0,0 @@
-import re
-from abc import abstractmethod
-from dataclasses import fields as dataclasses_fields, dataclass, is_dataclass, field as dataclass_field, Field
-from typing import Any, List
-
-
-
