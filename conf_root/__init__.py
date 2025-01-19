114e2d207f7eb843d05e3ed0bb3b561ac9f1b54a
ciaranchen <ciaranchen@qq.com>
2025-01-19 18:18:00 +0800

    移除Fields相关代码和测试。

--- conf_root/__init__.py
+++ conf_root/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 console_handler = logging.StreamHandler()
 formatter = logging.Formatter(
     fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,8 +11,6 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
 from .ConfRoot import ConfRoot
-from .Configuration import is_config_class, ChoiceField
-from .utils import ValidateException
 from .agents.BasicAgent import BasicAgent
 from .agents.JsonAgent import JsonAgent
 from .agents.YamlAgent import YamlAgent, SingleFileYamlAgent
